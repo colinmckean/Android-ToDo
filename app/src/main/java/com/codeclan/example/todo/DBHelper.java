823628388782b0b08824d48ffc707ae24c1d0626
Colin McKean <colinmckeandigital@gmail.com>
2017-03-03 18:57:21 +0000

    Add some fields for keys that will be required.

--- app/src/main/java/com/codeclan/example/todo/DBHelper.java
+++ app/src/main/java/com/codeclan/example/todo/DBHelper.java
@@ -1,8 +1,30 @@
 package com.codeclan.example.todo;
 
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by user on 03/03/2017.
  */
 
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper{
+
+    //database version, needs changed if schema updates.
+    public static final int DATABASE_VERSION = 1;
+
+    //database name.
+    public static final String DATABASE_NAME = "ToDoList.db";
+
+    // name of tables.
+    private static final String TABLE_TODO = "todos";
+    // common field id.
+    private static final String KEY_ID = "id";
+
+    // column names
+    private static final String KEY_TODO = "todo";
+    private static final String KEY_STATUS = "status";
+
+    //create to do table.
+    private static final String CREATE_TABLE_TODO = "CREATE TABLE "
+            + TABLE_TODO + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_TODO
+            + " TEXT," + KEY_STATUS + ")";
 }
