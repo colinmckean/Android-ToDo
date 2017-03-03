241abe6064803bfb880e6561a9c4cb4b09afbfdd
Colin McKean <colinmckeandigital@gmail.com>
2017-03-03 18:45:52 +0000

    Add constructors for ToDo

--- app/src/main/java/com/codeclan/example/todo/ToDo.java
+++ app/src/main/java/com/codeclan/example/todo/ToDo.java
@@ -8,10 +8,25 @@ public class ToDo {
 
     private int id;
     private int status;
-    private int listId;
-    private String title;
-    private String priorityId;
-    private String description;
+//    private int listId;
+//    private String title;
+//    private String priorityId;
+//    private String description;
     private String note;
+
+    //Add Constructors.
+    public ToDo() {
+    }
+
+    public ToDo(String note, int status) {
+        this.note = note;
+        this.status = status;
+    }
+
+    public ToDo(int id, String note, int status) {
+        this.id = id;
+        this.note = note;
+        this.status = status;
+    }
 
 }
