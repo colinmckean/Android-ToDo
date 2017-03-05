0e1903763a6f2f1a5cdafd11cb37d463e9b2d4fb
Colin McKean <colinmckeandigital@gmail.com>
2017-03-05 15:41:55 +0000

    add basic layout to edit todo

---
+++ app/src/main/java/com/codeclan/example/todo/EditToDoActivity.java
@@ -0,0 +1,14 @@
+package com.codeclan.example.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class EditToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_to_do);
+    }
+}
