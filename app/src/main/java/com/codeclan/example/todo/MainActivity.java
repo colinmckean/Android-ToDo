21d7ec7db20f7e8348744e7be4cc0cfe7e1fd3f9
Colin McKean <colinmckeandigital@gmail.com>
2017-03-07 12:01:34 +0000

    impletement menu on main activity so can view done, completed, and inprogress.

--- app/src/main/java/com/codeclan/example/todo/MainActivity.java
+++ app/src/main/java/com/codeclan/example/todo/MainActivity.java
@@ -4,11 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -18,64 +25,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     List<ToDo> inprogress;
     ListView ListOfToDos;
     String[] listItems;
+    ArrayAdapter<String> listAdapter;
+    TextView heading;
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewAllToDos = (Button) findViewById(R.id.viewAllToDos_Btn);
+        heading = (TextView) findViewById(R.id.mainActivityHeading_TextView);
         db = DBHelper.getInstance(getApplicationContext());
-
-
-        inprogress = db.getAllToDosByStatus(0);
-        listItems = getTitles(inprogress);
-
-        ListOfToDos = (ListView) findViewById(R.id.listByStatus_ListView);
-//
-        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
-        ListOfToDos.setAdapter(listAdapter);
-        ListOfToDos.setOnItemClickListener(this);
-//        ListName list1 = new ListName("Make more lists");
-//        ToDo todo1 = new ToDo("Create a Sample; Task", 1);
-//ToDo todo2 = new ToDo(-1, 2, "more to do");
-//ToDo toDo3 = new ToDo(-1, 2, "running out of idea");
-//        ToDo todo3 = new ToDo(1, "something");
-//        db.createList(list1);
-//        db.createToDo(todo1);
-//        db.createToDo(todo2);
-//        db.createToDo(toDo3);
-//        db.deleteToDo(24);
-//        todoToUpDate.setNote("THIS MUST BE SOMETHING AGAIN");
-//        todoToUpDate.setStatus(4);
-//        db.updateToDo(todoToUpDate);
-//        ToDo todoToUpDate = db.getTodo(2);
-
-//        Log.e("ToDo: ", "FRESH HOT OUTPUT");
-//        Log.e("Todo: ", "Found: " + db.getToDoCount() + " ToDo's");
-        List<ToDo> allLists = db.getAllToDos();
-        for (ToDo todo : allLists) {
-            Log.d("ToDo", todo.getNote() + " id: " + String.valueOf(todo.getId()) + "  " + todo.getStatus() + " " + todo.getListId());
-        }
-
-        //lets print out the list names
-//        Log.e("List", "**** printing out list names");
-//        ListName listNameToUpdate = db.getListName(1);
-//        listNameToUpdate.setList_name("UPDATEDS!");
-//        db.updateList(listNameToUpdate);
-//        List<ListName> allLists = db.getAllLists();
-//
-//        for(ListName listName : allLists){
-//            Log.d("List", "id" + listName.getId() + listName.getList_name());
-//        }
-
-
-//        ToDo todoToUpDate = db.getTodo(12);
-//         Log.d("ToDo Found", todoToUpDate.getNote() + " " + todoToUpDate.getStatus());
-//        todoToUpDate.setStatus(0);
-//        db.updateToDo(todoToUpDate);
-//        Log.d("ToDo Found", todoToUpDate.getNote() + " " + todoToUpDate.getStatus());
+        populateList(0);
     }
 
     public void viewLists(View view) {
@@ -96,9 +63,39 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        Log.d("something", "has been selected");
 
+        long id = inprogress.get(i).getId();
+        Intent intent = new Intent(this, ViewSingleToDo.class);
+        intent.putExtra("todo_id", id);
+        startActivity(intent);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.todo:
+                heading.setText(R.string.to_do);
+                populateList(0);
+                return true;
+            case R.id.inprogress:
+                heading.setText(R.string.in_progress);
+                populateList(1);
+                return true;
+            case R.id.done:
+                heading.setText(R.string.done);
+                populateList(-1);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void populateList(int status) {
+        inprogress = db.getAllToDosByStatus(status);
+        listItems = getTitles(inprogress);
+        ListOfToDos = (ListView) findViewById(R.id.listByStatus_ListView);
+        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
+        ListOfToDos.setAdapter(listAdapter);
+        ListOfToDos.setOnItemClickListener(this);
     }
 }
 
